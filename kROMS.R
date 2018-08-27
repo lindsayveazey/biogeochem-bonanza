b18c9703cc3b0e61f41fd7d7106ac7e6a5cd32c5
Lindsay Veazey <lindsaymveazey@gmail.com>
2018-08-27 11:32:09 -1000

    Added looping timeseries bash script from SZ

--- kROMS.R
+++ kROMS.R
@@ -15,3 +15,28 @@ v4 = nc$var[[4]] # salt
 v5 = nc$var[[5]] # temp
 v6 = nc$var[[6]] # u
 v7 = nc$var[[7]] # v
+
+### Bash script looping across 10 time series files;
+# avgs over each one, then
+# avgs over the 10 avgs
+# = one 10 month avg
+
+#!/bin/bash
+
+printf -v PATHin "/share/frinkraid/powellb/fleat/inner-nlm-w4d-2015-2016/output$
+printf -v PATHout "/share/frinkraid4/szedler/fleat/inner-nlm-w4d-2015-2016/anal$
+  
+  t0=5479
+for ((i=0; i<22; i++ ));
+do
+k=$((t0))
+echo $k
+printf -v FNin "%sfleat_inner_his_%05d.nc" $PATHin $k
+printf -v FNout "%stemp_%05d.nc" $PATHout $k
+echo $FNin
+echo $FNout
+ncks -d ocean_time,0,239 -v temp $FNin $FNout
+t0=$((t0+30))
+done
+cd $PATHout
+ncra temp_0*nc temp_mean.nc
