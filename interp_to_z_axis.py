0d7249ddb7271f56c305e3b5da8a50f7de6865ba
Lindsay Veazey <lindsaymveazey@gmail.com>
2018-11-28 09:27:19 -1000

    Final version
    Interpolate COBALT output from terrain-following axis to z-axis

--- interp_to_z_axis.py
+++ interp_to_z_axis.py
@@ -4,5 +4,5 @@ depths = np.array([1,10,50,100,125,150,200], dtype=object)
 nx = 4*4/111
 ny = 4*4/111
 seapy.roms.interp.to_zgrid('hiig_cobalt_avg.nc', 'out.nc', depth=depths, threads=4, nx=nx, ny=ny)
-# https://stackoverflow.com/questions/13717554/weird-behaviour-initializing-a-numpy-array-of-string-data ...didn't help.
 # https://stackoverflow.com/questions/28753572/python-error-file-stdin NO SPACES!
+
